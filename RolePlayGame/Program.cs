a137f9b791136e83e9c53ceef56b2a5f95f1651f
Danielle Skutela <Skutela97@gmail.com>
2020-10-22 11:52:08 +0100

    Added dotnet core project

---
+++ RolePlayGame/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace RolePlayGame
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
